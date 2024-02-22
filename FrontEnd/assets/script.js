01b17ea00f016e45ad927488e7a7ac73d6b0b40b
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2024-02-22 14:42:30 -0400

    Ajout des filtres de façon dynamique

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,17 +1,61 @@
+//Récupération des données "Projets" via l'API
 const reponse = await fetch("http://localhost:5678/api/works");
 const projets = await reponse.json();
 
-for (let i = 0; i < projets.length; i++) {
-    let projetImage = projets[i].imageUrl
-    let projetTitle = projets[i].title
+//Initialisation de la page
+let sectionGallery = document.querySelector(".gallery");
+affichageProjets(projets)
 
-    let figure = document.createElement("figure")
-    figure.innerHTML = `
-        <img src="${projetImage}" alt="${projetTitle}">
-        <figcaption>${projetTitle}</figcaption>
-        `;
 
-    let parentElement = document.querySelector(".gallery");
-    parentElement.appendChild(figure);
- }
+//Affichage des differents projets en fonction des filtres
+function affichageProjets(projetsfiltrer) {
 
+    for (let i = 0; i < projetsfiltrer.length; i++) {
+        let projetImage = projetsfiltrer[i].imageUrl
+        let projetTitle = projetsfiltrer[i].title
+    
+        let figure = document.createElement("figure")
+        figure.innerHTML = `
+            <img src="${projetImage}" alt="${projetTitle}">
+            <figcaption>${projetTitle}</figcaption>
+            `;
+
+        sectionGallery.appendChild(figure);
+     }
+
+}
+
+
+//gestion des bouttons
+const boutonTous = document.querySelector(".filtre_tous");
+boutonTous.addEventListener("click", function () {
+	sectionGallery.innerHTML = "";
+	affichageProjets(projets);
+});
+
+const boutonObjets = document.querySelector(".filtre_objets");
+boutonObjets.addEventListener("click", function () {
+	const projetsObjets = projets.filter(function (projet) {
+		return projet.categoryId === 1;
+	});
+	sectionGallery.innerHTML = "";
+	affichageProjets(projetsObjets);
+});
+
+const boutonAppart = document.querySelector(".filtre_appart");
+boutonAppart.addEventListener("click", function () {
+	const projetsAppart = projets.filter(function (projet) {
+		return projet.categoryId === 2;
+	});
+	sectionGallery.innerHTML = "";
+	affichageProjets(projetsAppart);
+});
+
+const boutonHotel = document.querySelector(".filtre_hotel");
+boutonHotel.addEventListener("click", function () {
+	const projetsHotel = projets.filter(function (projet) {
+		return projet.categoryId === 3;
+	});
+	sectionGallery.innerHTML = "";
+	affichageProjets(projetsHotel);
+});
